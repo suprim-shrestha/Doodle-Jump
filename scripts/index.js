25f169c6ae4cb771c76538642bf570d1ee34f39a
Suprim Shrestha <suprim.shresth@gmail.com>
2023-12-03 18:29:29 +0545

    Move platform related functions to new file

--- scripts/index.js
+++ scripts/index.js
@@ -36,43 +36,6 @@ let maxScore = 0;
 let gameOver = false;
 
 /**
- * Create a platform above last platform and adds it to platform array
- */
-function createPlatform() {
-  const platformX = Math.floor(getRandomNum(0, canvas.width - platformWidth));
-  const platformY =
-    platformArray[platformArray.length - 1].y -
-    getRandomNum(minPlatformDistance, maxPlatformDistance);
-  platform = new Platform(platformX, platformY, platformWidth, platformHeight);
-  platformArray.push(platform);
-}
-
-/**
- * Create first 10 platforms
- */
-function createInitialPlatforms() {
-  let platform = new Platform(
-    canvas.width / 2 - platformWidth / 2,
-    canvas.height - 50,
-    platformWidth,
-    platformHeight
-  );
-  platformArray.push(platform);
-  for (let i = 0; i < 9; i++) {
-    createPlatform();
-  }
-}
-
-/**
- * Create new platform when last platform is visible on screen
- */
-function createNewPlatforms() {
-  if (platformArray[platformArray.length - 1].y >= 0) {
-    createPlatform();
-  }
-}
-
-/**
  * Set all values to default and restarts game
  */
 function resetGame() {
