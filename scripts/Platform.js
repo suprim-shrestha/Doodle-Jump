cc7891a0c410eb866633ae92555eaa83362c5cef
Suprim Shrestha <suprim.shresth@gmail.com>
2023-12-03 10:59:42 +0545

    Create Platform class

---
+++ scripts/Platform.js
@@ -0,0 +1,30 @@
+const platformImage = new Image();
+platformImage.src = "../assets/game-tiles.png";
+
+class Platform {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.color = "#333";
+  }
+
+  draw(ctx) {
+    // ctx.fillStyle = this.color;
+    // ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    ctx.drawImage(
+      platformImage,
+      1,
+      1,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
